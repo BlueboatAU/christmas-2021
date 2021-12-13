95a2003daf61d619ba9906b1096fd8eeb7a4ae4b
@guyaldous1 <guyaldous@gmail.com>
2021-12-13 14:20:24 +1100

    crap browser test

---
+++ src/js/_app/crapBrowsers.js
@@ -0,0 +1,18 @@
+export const crapBrowsers = () => {
+
+    if (/MSIE 10/i.test(navigator.userAgent)) {
+        // This is internet explorer 10
+        window.alert('isIE10');
+     }
+     
+     if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+         // This is internet explorer 9 or 11
+         window.location = 'pages/core/ie.htm';
+     }
+     
+     if (/Edge\/\d./i.test(navigator.userAgent)){
+        // This is Microsoft Edge
+        window.alert('Microsoft Edge');
+     }
+
+}
--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,13 @@ import {waitForFinalEvent, matches} from "./_app/helpers.js"
 import { setupMultiCanvas } from './_app/multiCanvas';
 import { followCursor } from './_app/cursor';
 import { blotterSetup } from './_app/blotter'
+import {crapBrowsers} from './_app/crapBrowsers.js'
 
 //run all init scripts
 const loadHandler = () => {
 
   followCursor()
+  crapBrowsers()
 
 }
 
